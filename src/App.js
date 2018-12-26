cd8a1211fd67112375297a32fd98fcce4576a581
heidiulery <heidiulery@hotmail.com>
2018-12-26 15:49:39 -0600

    Events Checkpoint: completed

--- src/App.js
+++ src/App.js
@@ -10,8 +10,27 @@ class App extends Component {
         { description: 'Walk the cat', isCompleted: true },
         { description: 'Throw the dishes away', isCompleted: false },
         { description: 'Buy new dishes', isCompleted: false }
-      ]
+      ],
+      newTodoDescription: ''
     };
+  }
+
+  toggleComplete(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newTodoDescription) { return }
+    const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+    this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' }); 
+  }
+
+  handleChange(e) {
+    this.setState({ newTodoDescription: e.target.value })
   }
 
   render() {
@@ -23,9 +42,18 @@ class App extends Component {
               key={ index } 
               description={ todo.description }
               isCompleted={ todo.isCompleted }
+              toggleComplete={ () => this.toggleComplete(index) }
             />
           )}
         </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input 
+            type="text" 
+            value={ this.state.newTodoDescription } 
+            onChange={ (e) => this.handleChange(e) } 
+          />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class ToDo extends Component {
   render() {
+    const toggle = this.props.toggleComplete;
     return (
       <li>
-        <input type="checkbox" checked={ this.props.isCompleted } />
+        <input 
+          type="checkbox" 
+          checked={ this.props.isCompleted } 
+          onChange={ toggle }
+        />
         <span>{ this.props.description }</span>
       </li>
     );
